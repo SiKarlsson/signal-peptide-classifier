a430d87d23c15468f7945c1bb79701b9fc70ba73
sikar <simonkarlsson@msn.com>
2017-01-03 11:01:12 +0100

    Add SVM classifier with rbf-kernel

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,10 +1,14 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
-def train(positiveSamples, negativeSamples):
+def train(positiveSamples, negativeSamples, method):
     samples = positiveSamples + negativeSamples
     targets = ([1] * len(positiveSamples)) + ([0] * len(negativeSamples))
     count = CountVectorizer(analyzer='char', lowercase=False).fit(samples)
     tfidf = TfidfTransformer().fit_transform(count.transform(samples))
-    return MultinomialNB().fit(tfidf, targets), count
+    if method == "nb":
+        return MultinomialNB().fit(tfidf, targets), count
+    elif method =="svc":
+        return SVC(kernel="rbf").fit(tfidf, targets), count
